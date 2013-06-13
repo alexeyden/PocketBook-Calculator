43fd092b063d6e89f467fbe8c9c4842a4e637425
Alexey Denisov <rtgbnm@gmail.com>
2013-06-13 17:53:18 +0600

    FullscreenList bugfix

---
+++ CMakeFiles/ecalc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ecalc.dir/src/fparser.cc.o"
+  "CMakeFiles/ecalc.dir/src/main.cpp.o"
+  "ecalc.pdb"
+  "ecalc"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ecalc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
